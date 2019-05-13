c4c5b143c8f241f4f9fd453dc70a8d45fff273a4
ohjinseok <ohjinseok1113@gmail.com>
2019-05-13 11:22:09 +0900

    setting urls of account & complete signup

--- account/views.py
+++ account/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return render('accounts:detail')
+    else:
+        form = CustomUserCreationForm()
+        return render(request, 'account/signup.html', {'form': form})
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'account:detail')
+    else:
+        form = AuthenticationForm()
+        return render(request, "account/login.html", {'form': form})
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect("movie:index")
