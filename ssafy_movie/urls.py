6188c23d7bd6db252cce2034eaf5919275239585
dowookims <kaei2574@gmail.com>
2019-05-13 13:31:54 +0900

    Add readMe and static files and Edit settings.py to set static route

--- ssafy_movie/urls.py
+++ ssafy_movie/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
+from movie import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('account.urls')),
     path('movies/', include('movie.urls')),
-]
+    path('', views.index, name='root')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
