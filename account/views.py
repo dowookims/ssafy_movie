8ee54790168557dc41f460e7553609104dc5f30e
ohjinseok <ohjinseok1113@gmail.com>
2019-05-14 02:05:30 +0900

    Add user detail

--- account/views.py
+++ account/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model, update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, PasswordChangeForm
 from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 # from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -32,3 +34,9 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect("movie:index")
+
+
+@login_required
+def detail(request, username):
+    person = get_object_or_404(get_user_model(), username=username)
+    return render(request, 'account/detail.html', {'person': person})
