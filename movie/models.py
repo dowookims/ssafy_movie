7eb99fcf777b8e4e02624b7f58afd26c05e01c7b
ohjinseok <ohjinseok1113@gmail.com>
2019-05-13 15:27:50 +0900

    Add Movie model & Movie data

--- movie/models.py
+++ movie/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.CharField(max_length=300)
+    pubDate = models.IntegerField()
+    director = models.CharField(max_length=200)
+    actor = models.CharField(max_length=200)
+    userRating = models.FloatField()
+    subtitle = models.CharField(max_length=200)
+    link = models.CharField(max_length=300)
+
