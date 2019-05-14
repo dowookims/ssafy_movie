2e4c108b16200637a287553b79747a78f9bbff55
dowookims <kaei2574@gmail.com>
2019-05-14 17:48:01 +0900

    Edit some css and js

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,16 @@
+from movie.models import Movie, Genre
 from rest_framework import serializers
-from movie.models import Movie
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['name']
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(read_only=True, many=True)
+
     class Meta:
         model = Movie
-        fields = ['title', 'image', 'pubDate', 'userRating', ]
+        fields = "__all__"
