0e8b136d6dd1afc8064d0dedfa1caae11784a656
dowookims <kaei2574@gmail.com>
2019-05-16 12:28:35 +0900

    Commit before pull

--- movie/static/movie/js/recommend.js
+++ movie/static/movie/js/recommend.js
@@ -1,26 +1,51 @@
 Vue.component('recommends', {
-  props: ['rcmv'],
+  props: ['movieid'],
   data: function(){
     return {
+      rendered: false,
       origin_url: "https://image.tmdb.org/t/p/original",
       w500_url: "https://image.tmdb.org/t/p/w500",
+      recommendMovies:[{
+        title:'',
+        backdrop_path:''
+      }]
     }
   },
+  methods: {
+    prevPage:function(){
+      console.log('work well left')
+    },
+    nextPage: function(){
+      console.log('work well right')
+    }
+} ,
   mounted: function(){
-    this.mv = this.rcmv
+    console.log('recommand mount')
+    axios.get(`https://api.themoviedb.org/3/movie/${this.movieid}/similar?api_key=${API_KEY}&language=ko-KR`)
+        .then(res => {
+          this.recommendMovies= []
+          return(res.data.results)
+        })
+        .then(data => {
+          data.forEach(movie => {
+            this.recommendMovies.push(movie)
+          })
+        })
   },
 
   template: `
   <div class="recommend-list">
-    <div class="recommend-mv-list>
-      <div v-for="movie in rcmv">
+  <i @click="prevPage" class="fas fa-chevron-left arrow left-arrow2"></i>
+    <div class="recommend-mv-list">
+      <div class="rc-mv-item" v-for="mv in recommendMovies">
         <div class="recMvImg">
-          <img :src="w500_url+movie.backdrop_path" />
+          <img class="recMvImage":src="w500_url+mv.backdrop_path" />
         </div>
         <div class="recMvSub">
-        <p>{{movie.title}}</p>
+        <p>{{mv.title}}</p>
         </div>
       </div>
+      <i @click="nextPage"class="fas fa-chevron-right arrow right-arrow2"></i>
     </div>
   </div>
   `
